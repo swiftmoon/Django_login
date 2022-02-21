fa40b2aa2975debfcc6cb2b1bcfee47f3a7db024
Asadbek <softwareengineer2022@mail.com>
2022-02-21 15:50:42 +0500

    login

--- .env/bin/activate.fish
+++ .env/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/asadbek/django/django_page/.env"
+set -gx VIRTUAL_ENV "/home/asadbek/django/django_login/.env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
